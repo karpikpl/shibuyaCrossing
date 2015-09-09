5702027c68b259bf502bf511248d20c3b283f650
Piotr Karpala <pkarpala@infusion.com>
2015-09-09 22:07:10 +0200

    removed branching to [problem]

--- setup.ps1
+++ setup.ps1
@@ -24,8 +24,8 @@ Rename-Item KattisSolution.sln $newSlnName
 $newSubmitScript = "python submit.py -p" + $args[0] +" KattisSolution\Program.cs KattisSolution\InputOutput.cs"
 New-Item "submitMe.bat" -type file -force -value $newSubmitScript
 
-$gitCommand = "checkout -b " + $args[0]
-Start-Process git -ArgumentList $gitCommand -wait -NoNewWindow -PassThru
+# $gitCommand = "checkout -b " + $args[0]
+# Start-Process git -ArgumentList $gitCommand -wait -NoNewWindow -PassThru
 Start-Process git -ArgumentList "add --all --verbose" -wait -NoNewWindow
 Start-Process git -ArgumentList "commit -m ""setup commit"" --verbose" -wait -NoNewWindow -PassThru
 
