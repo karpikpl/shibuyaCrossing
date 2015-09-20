cb5ff210f28e1f432e49b2ff5adfbdb8142d96e6
Piotr Karpala <pkarpala@infusion.com>
2015-09-21 01:10:18 +0200

    test case not working but units pass :/

--- KattisSolution/Program.cs
+++ KattisSolution/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using KattisSolution.IO;
 
 namespace KattisSolution
@@ -19,11 +22,109 @@ namespace KattisSolution
             // scanner = new LineReader(stdin);
             var writer = new BufferedStdoutWriter(stdout);
 
-            var input = scanner.NextInt();
+            var n = scanner.NextInt();
+            var m = scanner.NextInt();
 
-            writer.Write(input*5);
+            if (n == 1)
+            {
+                writer.Write(1);
+                writer.Write("\n");
+                writer.Flush();
+                return;
+            }
+
+            var meetings = new Dictionary<int, HashSet<int>>(n);
+
+            for (int i = 0; i < m; i++)
+            {
+                int a = scanner.NextInt();
+                int b = scanner.NextInt();
+
+                HashSet<int> forA;
+                if (meetings.ContainsKey(a))
+                {
+                    forA = meetings[a];
+
+                    if (!forA.Contains(b))
+                        forA.Add(b);
+                }
+                else
+                {
+                    forA = new HashSet<int> { b };
+                    meetings.Add(a, forA);
+                }
+
+                HashSet<int> forB;
+                if (meetings.ContainsKey(b))
+                {
+                    forB = meetings[b];
+
+                    if (!forB.Contains(a))
+                        forB.Add(a);
+                }
+                else
+                {
+                    forB = new HashSet<int> { a };
+                    meetings.Add(b, forB);
+                }
+            }
+
+            var result = 0;
+
+            foreach (var meeting in meetings.OrderByDescending(g => g.Value.Count))
+            {
+                // we know that for key all connections are there
+                var everyoneBumped = new HashSet<int> { meeting.Key };
+                Debug.WriteLine("Checking " + meeting.Key + " with: " + string.Join(", ", meeting.Value));
+
+                for (int j = 0; j < meeting.Value.Count; j++)
+                {
+                    var personA = meeting.Value.ElementAt(j);
+
+                    for (int i = j + 1; i < meeting.Value.Count; i++)
+                    {
+                        var personB = meeting.Value.ElementAt(i);
+                        //Debug.WriteLine("Looking for {0} =? {1}", personA, personB);
+                        if (!meetings[personA].Contains(personB))
+                        {
+                            //Debug.WriteLine("{0} <-> {1} not found", personA, personB);
+                            personA = -1;
+                            break;
+                        }
+                    }
+
+                    if (personA > 0)
+                        everyoneBumped.Add(personA);
+                }
+
+                Debug.WriteLine("Found group: " + string.Join(", ", everyoneBumped));
+                result = Math.Max(everyoneBumped.Count, result);
+            }
+
+            writer.Write(result);
             writer.Write("\n");
             writer.Flush();
+        }
+
+        public class Group
+        {
+            public Group(int a, int b)
+            {
+                Meetings = 1;
+                People = new HashSet<int> { a, b };
+            }
+
+            public readonly HashSet<int> People;
+            public int Meetings;
+
+            public void Add(int a, int b)
+            {
+                if (!People.Contains(a))
+                    People.Add(a);
+                if (!People.Contains(b))
+                    People.Add(b);
+                Meetings++;
+            }
         }
     }
 }
--- KattisSolution/Tests/CustomTest.cs
+++ KattisSolution/Tests/CustomTest.cs
@@ -4,7 +4,6 @@ using NUnit.Framework;
 
 namespace KattisSolution.Tests
 {
-    [Ignore]
     [TestFixture]
     [Category("sample")]
     public class CustomTest
@@ -13,8 +12,151 @@ namespace KattisSolution.Tests
         public void SampleTest_WithStringData_Should_Pass()
         {
             // Arrange
-            const string expectedAnswer = "50\n";
-            using (var input = new MemoryStream(Encoding.UTF8.GetBytes("10\n")))
+            const string expectedAnswer = "13\n";
+            StringBuilder inputString = new StringBuilder("800 9588\n");
+            for (int i = 1; i <= 12; i++)
+                for (int j = 1; j <= 800; j++)
+                {
+                    if (i != j)
+                    {
+                        inputString.Append(i).Append(" ").Append(j).Append("\n");
+                    }
+                }
+
+
+            using (var input = new MemoryStream(Encoding.UTF8.GetBytes(inputString.ToString())))
+            using (var output = new MemoryStream())
+            {
+                // Act
+                Program.Solve(input, output);
+                var result = Encoding.UTF8.GetString(output.ToArray());
+
+                // Assert
+                Assert.That(result, Is.EqualTo(expectedAnswer));
+            }
+        }
+
+        [Test]
+        public void SampleTest_WithStringData_Should_Pass2()
+        {
+            // Arrange
+            const string expectedAnswer = "2\n";
+            StringBuilder inputString = new StringBuilder("800 799\n");
+            for (int i = 1; i <= 800; i++)
+            {
+                inputString.Append("1 ").Append(i).Append("\n");
+            }
+
+
+            using (var input = new MemoryStream(Encoding.UTF8.GetBytes(inputString.ToString())))
+            using (var output = new MemoryStream())
+            {
+                // Act
+                Program.Solve(input, output);
+                var result = Encoding.UTF8.GetString(output.ToArray());
+
+                // Assert
+                Assert.That(result, Is.EqualTo(expectedAnswer));
+            }
+        }
+
+        [Test]
+        public void SampleTest_WithStringData_Should_Pass_When_OnePerson()
+        {
+            // Arrange
+            const string expectedAnswer = "1\n";
+
+            using (var input = new MemoryStream(Encoding.UTF8.GetBytes("1 0\n")))
+            using (var output = new MemoryStream())
+            {
+                // Act
+                Program.Solve(input, output);
+                var result = Encoding.UTF8.GetString(output.ToArray());
+
+                // Assert
+                Assert.That(result, Is.EqualTo(expectedAnswer));
+            }
+        }
+
+        [Test]
+        public void SampleTest_WithStringData_Should_Pass_When_ZereBumps()
+        {
+            // Arrange
+            const string expectedAnswer = "0\n";
+
+            using (var input = new MemoryStream(Encoding.UTF8.GetBytes("2 0\n")))
+            using (var output = new MemoryStream())
+            {
+                // Act
+                Program.Solve(input, output);
+                var result = Encoding.UTF8.GetString(output.ToArray());
+
+                // Assert
+                Assert.That(result, Is.EqualTo(expectedAnswer));
+            }
+        }
+
+        [Test]
+        public void SampleTest_WithStringData_Should_Pass5()
+        {
+            // Arrange
+            const string expectedAnswer = "2\n";
+
+            using (var input = new MemoryStream(Encoding.UTF8.GetBytes("3 2\n1 2\n2 3\n")))
+            using (var output = new MemoryStream())
+            {
+                // Act
+                Program.Solve(input, output);
+                var result = Encoding.UTF8.GetString(output.ToArray());
+
+                // Assert
+                Assert.That(result, Is.EqualTo(expectedAnswer));
+            }
+        }
+
+        [Test]
+        public void SampleTest_WithStringData_Should_Pass6()
+        {
+            // Arrange
+            const string expectedAnswer = "5\n";
+
+            using (var input = new MemoryStream(Encoding.UTF8.GetBytes("9 15\n1 7\n3 9\n2 9\n1 2\n2 3\n5 6\n1 5\n1 4\n1 6\n4 6\n5 7\n6 7\n4 5\n4 7\n1 3\n")))
+            using (var output = new MemoryStream())
+            {
+                // Act
+                Program.Solve(input, output);
+                var result = Encoding.UTF8.GetString(output.ToArray());
+
+                // Assert
+                Assert.That(result, Is.EqualTo(expectedAnswer));
+            }
+        }
+
+        [Test]
+        public void SampleTest_WithStringData_Should_Pass7()
+        {
+            // Arrange
+            const string expectedAnswer = "3\n";
+
+            using (var input = new MemoryStream(Encoding.UTF8.GetBytes("9 13\n1 7\n3 9\n2 9\n1 2\n2 3\n5 6\n1 4\n1 6\n5 7\n6 7\n4 5\n4 7\n1 3\n")))
+            using (var output = new MemoryStream())
+            {
+                // Act
+                Program.Solve(input, output);
+                var result = Encoding.UTF8.GetString(output.ToArray());
+
+                // Assert
+                Assert.That(result, Is.EqualTo(expectedAnswer));
+            }
+        }
+
+        [Test]
+        public void SampleTest_WithStringData_Should_Pass8()
+        {
+            // Arrange
+            const string expectedAnswer = "2\n";
+
+            using (var input = new MemoryStream(Encoding.UTF8.GetBytes("9 10\n1 7\n3 9\n2 9\n1 2\n5 6\n1 4\n1 6\n5 7\n4 5\n1 3\n")))
             using (var output = new MemoryStream())
             {
                 // Act
